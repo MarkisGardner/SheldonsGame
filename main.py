40ae7aef3c0407233062b5689b7e6d1c592e4424
MarkisGardner <109382615+MarkisGardner@users.noreply.github.com>
2024-06-12 10:38:29 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def instructions:
+    print("Rock wins against:")
+    print("    Scissors")
+    print("    Lizard")
+    print("Paper wins against:")
+    print("    Rock")
+    print("    Spock")
+    print("Scissors wins against:")
+    print("    Paper")
+    print("    Lizard")
+    print("Lizard wins against:")
+    print("    Paper")
+    print("    Spock")
+    print("Spock wins against:")
+    print("    Scissors")
+    print("    Rock")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
