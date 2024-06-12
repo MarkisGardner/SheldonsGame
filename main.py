7f3ee87bd0b00ba897385a2da7cabaad4d5f1e7e
MarkisGardner <109382615+MarkisGardner@users.noreply.github.com>
2024-06-12 11:53:53 -0400

    Added some code to see who wins.  Finish tomorrow

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
-def instructions:
+
+def instructions():
     print("Rock wins against:")
     print("    Scissors")
     print("    Lizard")
@@ -19,13 +21,116 @@ def instructions:
     print("Spock wins against:")
     print("    Scissors")
     print("    Rock")
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def winning_info(result):
+    print("You Win!")
+
+
+def did_you_win(your_choice, computer_choice):
+    if your_choice == "R":
+        if computer_choice == 3:
+            print("Your Rock crushes the computer's scissors!  You win!")
+            return "W"
+        elif computer_choice == 4:
+            print("Your Rock crushes the computer's lizard!  You win! ")
+            return "W"
+        elif computer_choice == 5:
+            print("The computer's Spock vaporizes your rock!  You lose!")
+            return "L"
+        elif computer_choice == 2:
+            print("The computer's Paper covers your rock!  You lose!")
+            return "L"
+        else:
+            print("It is a tie - you both chose the same!")
+            return "T"
+    elif your_choice == "P":
+        if computer_choice == 3:
+            print("Your Paper gets cut by the computer's scissors!  You lose!")
+            return "L"
+        elif computer_choice == 4:
+            print("Your Paper gets eaten by the computer's lizard!  You lose! ")
+            return "L"
+        elif computer_choice == 5:
+            print("Your paper disproves Spock!  You win!")
+            return "W"
+        elif computer_choice == 1:
+            print("Your rock covers the computer's rock!  You win!")
+            return "W"
+        else:
+            print("It is a tie - you both chose the same!")
+            return "T"
+#start here tomorrow.
+    elif your_choice == "R":
+        if computer_choice == 3:
+            print("Your Rock crushes the computer's scissors!  You win!")
+            return "W"
+        elif computer_choice == 4:
+            print("Your Rock crushes the computer's lizard!  You win! ")
+            return "W"
+        elif computer_choice == 5:
+            print("The computer's Spock vaporizes your rock!  You lose!")
+            return "L"
+        elif computer_choice == 2:
+            print("The computer's Paper covers your rock!  You lose!")
+            return "L"
+        else:
+            print("It is a tie - you both chose the same!")
+            return "T"
+    elif your_choice == "R":
+        if computer_choice == 3:
+            print("Your Rock crushes the computer's scissors!  You win!")
+            return "W"
+        elif computer_choice == 4:
+            print("Your Rock crushes the computer's lizard!  You win! ")
+            return "W"
+        elif computer_choice == 5:
+            print("The computer's Spock vaporizes your rock!  You lose!")
+            return "L"
+        elif computer_choice == 2:
+            print("The computer's Paper covers your rock!  You lose!")
+            return "L"
+        else:
+            print("It is a tie - you both chose the same!")
+            return "T"
+    elif your_choice == "R":
+        if computer_choice == 3:
+            print("Your Rock crushes the computer's scissors!  You win!")
+            return "W"
+        elif computer_choice == 4:
+            print("Your Rock crushes the computer's lizard!  You win! ")
+            return "W"
+        elif computer_choice == 5:
+            print("The computer's Spock vaporizes your rock!  You lose!")
+            return "L"
+        elif computer_choice == 2:
+            print("The computer's Paper covers your rock!  You lose!")
+            return "L"
+        else:
+            print("It is a tie - you both chose the same!")
+            return "T"
+    else:
+        print("You can't follow instructions - you lose!")
+        instructions()
+        return "L"
+
+def play_game(times):
+    for i in range(times):
+        print("Enter your choice:  (R)ock, (P)aper, (S)cissors, (L)izard, (Sp)ock")
+        your_try = input()
+        number = random.randint(1, 5)
+        print("I is ", i)
+        win = did_you_win(your_try, number)
+        tally[i] = win
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Please enter the number of rounds you want play or enter I for instructions.")
+ans = int(input())
+# Note: I am not going to do much testing to make sure they enter a number or I.
+if ans == 'I':
+    instructions()
+    print("Thank you for playing!")
+    quit(0)
+else:
+    play_game(ans)
